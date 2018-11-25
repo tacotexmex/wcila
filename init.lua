ff4346823f0a59c8394636271ca2e976111c95c0
tacotexmex <josefsahlberg+minetest@gmail.com>
2018-11-25 15:51:00 +0100

    Refactor mod and document code

--- init.lua
+++ init.lua
@@ -1,115 +1,105 @@
-pointlib = {}
-pointlib.huds = {}
-local show_image = true
+-- Create public mod table
+pointlib = {
+	hud = {}
+}
 
---Check pointlib Visibility
-local function pointlib_visible(node, player)
+-- Check pointlib Visibility
+local function visible(node, player)
 	--To prevent a crash from unknown nodes
 	local def = minetest.registered_items[node]
 	if def == nil then return false end
-
-	--Don't show air!
+	-- Don't show air!
 	if def.name == "air" then return false end
-
-	--Check if the Player is holding down the sneak key, if they are then show all nodes
+	-- Check if the Player is holding down the sneak key, if they are then show all nodes
 	if not player:get_player_control().sneak then
-		if def.drawtype == "liquid" or def.drawtype == "flowingliquid" or def.drawtype == "airlike" then return false end
+		if def.drawtype == "liquid"
+		or def.drawtype == "flowingliquid"
+		or def.drawtype == "airlike" then
+			return false
+		end
 	end
-
-	--Make sure the node hasn't requested to be hidden
-	if def.groups.not_pointlib_visible and def.groups.not_pointlib_visible ~= 0 then return false end
+	-- Make sure the node hasn't requested to be hidden
+	if def.groups.not_pointlib_visible
+	and def.groups.not_pointlib_visible ~= 0 then
+		return false
+	end
+	-- If def passes these checks then node is visible
 	return true
 end
 
---Create pointlib Hud
-local function create_pointlib_hud(player)
-	local elems = {}
-	elems.bg = player:hud_add({
-		hud_elem_type = "image",
-		position = {x = 0.5, y = 0},
-		scale = {x = 0, y = 0},
-		name = "pointlib Background",
-		text = "pointlib_bg.png",
-		alignment = 0,
-		offset = {x = 0, y = 40},
-		direction = 0
-	})
-	elems.tooltip = player:hud_add({
-		hud_elem_type = "text",
-		position = {x = 0.5, y = 0},
-		scale = {x = 100, y = 100},
-		number = 0xFFFFFF,
-		alignment = 0,
-		offset = {x = 0, y = 26},
-		direction = 0,
-		name = "pointlib Display Name",
-		text = "",
-	})
-	elems.technical = player:hud_add({
-		hud_elem_type = "text",
-		position = {x = 0.5, y = 0},
-		scale = {x = 100, y = 100},
-		number = 0xCCCCCC,
-		alignment = 0,
-		offset = {x = 0, y = 46},
-		direction = 0,
-		name = "pointlib Technical Name",
-		text = "",
-	})
-
-	pointlib.huds[player:get_player_name()] = elems
-end
-
---Detect and show block
+-- Check for closest visible node in ray and update HUD accordingly
 function pointlib.update(player)
-	local name
-	local dir = player:get_look_dir()
+	-- Get player position
 	local pos = vector.add(player:getpos(),{x=0,y=1.625,z=0})
-
-	--TODO Go back to old method, it gives more flexibility
-	-- local has_sight, node_pos = minetest.line_of_sight(pos, vector.add(pos,vector.multiply(dir,40)),0.3)
-
+	-- Get player view direction
+	local dir = player:get_look_dir()
+	-- Cast a ray in this direction
 	local ray = minetest.raycast(pos, vector.add(pos,vector.multiply(dir,4)), false, true)
-	local name = ""
+	-- Create variable of node name of possible outcome
+	local name
+	-- Step through ray
 	for pointed_thing in ray do
-		local cname = minetest.get_node(pointed_thing.under).name
-		if pointlib_visible(cname, player) then name = cname break end
+		-- Create variable for nodes found in ray
+		local name_in_ray = minetest.get_node(pointed_thing.under).name
+		-- Check if node should be ignored or not
+		if visible(name_in_ray, player) then
+			-- If so, put it in node name outcome variable
+			name = name_in_ray
+			-- No need to step further in ray
+			break
+		end
 	end
-
-	local display_name = ""
-	if name ~= "" and minetest.registered_items[name].description ~= "" then display_name = minetest.registered_items[name].description end
-	local s = 0
-	--local techoff = 48
-	if name ~= "" then s = 1 end
-	--if display_name == "" then techoff = 38 end
-
-	if not pointlib.huds[player:get_player_name()] then
-		create_pointlib_hud(player)
-	else
-		local elems = pointlib.huds[player:get_player_name()]
-		player:hud_change(elems.bg, "scale", {x = s * 3, y = s * 3})
-		player:hud_change(elems.tooltip, "text", display_name)
-		player:hud_change(elems.technical, "text", name)
+	-- If both name and description, update HUD
+	if name and minetest.registered_items[name].description ~= "" then
+		-- Get node description
+		local description = minetest.registered_items[name].description
+		-- Update name HUD
+		player:hud_change(pointlib.hud.name, "text", name)
+		-- Update description HUD
+		player:hud_change(pointlib.hud.description, "text", description)
+		-- Return pointed node to external API function
 		return name
 	end
 end
 
--- Register Update
-local time = 0
-local incr = 0.1
-minetest.register_globalstep(function(dtime)
-	time = time + dtime
-	if time > incr then
-		time = time - incr
-		for _,player in ipairs(minetest.get_connected_players()) do
-			if player and player:is_player() then
-				pointlib.update(player)
-			end
-		end
-	end
+-- Create HUD for new players
+minetest.register_on_joinplayer(function (player)
+	-- Create HUD element for node description
+	pointlib.hud.description = player:hud_add({
+		name = "pointlib:description",
+		position = {x=0.5,y=0},
+		hud_elem_type = "text",
+		number = 0xFFFFFF,
+		alignment = 0,
+		offset = { x = 0, y = 20},
+		text = ""
+	})
+	-- Create HUD element for node name
+	pointlib.hud.name = player:hud_add({
+		name = "pointlib:name",
+		position = {x=0.5,y=0},
+		hud_elem_type = "text",
+		number = 0xFFFFFF,
+		alignment = 0,
+		offset = { x = 0, y = 40},
+		text = ""
+	})
 end)
 
---Register Leave Clearing
-minetest.register_on_leaveplayer(function (player)
-	pointlib.huds[player:get_player_name()] = nil
+-- Create timer variable
+local timer = 0
+-- Create loop for updating frequency
+minetest.register_globalstep(function (dtime)
+	-- Iterate on timer with past time
+	timer = timer + dtime
+	-- Do things when 200 milliseconds have passed
+	if timer > 0.2 then
+		-- Check for all online players
+		for _,player in pairs(minetest:get_connected_players()) do
+			-- Update all player's HUDs
+			pointlib.update(player)
+		end
+		-- Reset timer
+		timer = 0
+	end
 end)
